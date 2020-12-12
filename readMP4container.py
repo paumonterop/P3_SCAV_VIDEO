9413750bf3c684275e6fcf722cea1002ccd26da1
pau <pautorello@gmail.com>
2020-12-12 11:48:17 +0100

    bug corrected

--- readMP4container.py
+++ readMP4container.py
@@ -11,38 +11,36 @@ args = parser.parse_args()
 
 
 def broadcastFit(filename): # guardem codecs
-    cmnd = ['ffprobe', '-show_format', '-show_streams', '-loglevel', 'quiet', filename]
-    p = subprocess.Popen(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
     print('Broacdasting Standards compatibles amb', filename)
-    out, err = p.communicate()
-    out_string = out.decode()
-    outstring = out_string.split('\n')
-    codec_video = outstring[2]
-    codec_audio = outstring[59]
+    codec_video = subprocess.getoutput('ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of '
+                                       'default=nokey=1:noprint_wrappers=1 {}'.format(filename))
+    codec_audio = subprocess.getoutput('ffprobe -v error -select_streams a:0 -show_entries stream=codec_name -of '
+                                       'default=nokey=1:noprint_wrappers=1 {}'.format(filename))
     # print(codec_video)
     # print(codec_audio)
 
-# comaprem els codecs del video amb els standards
-    if codec_video == 'codec_name=h264' or 'codec_name=mpeg2':
+    # comparem els codecs del video amb els standards
+    if codec_video == 'h264' or 'mpeg2':
 
-        if codec_audio == 'codec_name=mp3':
+        if codec_audio == 'mp3':
             broadcast = 'Compatible amb: DVB i DTMB'
             print(broadcast)
 
-        elif codec_audio == 'codec_name=acc':
+        elif codec_audio == 'aac':
             broadcast = 'Compatible amb: DVB i ISDB i DTMB'
             print(broadcast)
 
-        elif codec_audio == 'codec_name=ac-3':
+        elif codec_audio == 'ac-3':
             broadcast = 'Comaptible amb: DVB i ATSC i DTMB'
             print(broadcast)
 
         else:
             print('No és compatible amb cap Standard')
 
-    elif codec_video == 'codec_name=avs' or 'codec_name=avs+':
+    elif codec_video == 'avs' or 'avs+':
 
-        if codec_audio == 'codec_name=mp2' or 'codec_name=dra':
+        if codec_audio == 'mp2' or 'dra':
             broadcast = 'Compatible amb: DTMB'
             print(broadcast)
 
